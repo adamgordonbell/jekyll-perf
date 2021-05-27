e0c179cc57760b7c766a8d4060ccfba6cf7dd64f
Adam Gordon Bell <adam@corecursive.com>
2021-05-27 12:03:41 -0400

    clean up test script"

--- test-two-seven/test.sh
+++ test-two-seven/test.sh
@@ -5,6 +5,6 @@ bundle exec jekyll serve --incremental -H 0.0.0.0 -P 4001 &
 sleep 20
 for i in $(seq 1 20)
 do
-   echo "$i\n" > ./_posts/2009-05-15-edge-case-nested-and-mixed-lists.md
+   echo "$i\n" >> ./_posts/2009-05-15-edge-case-nested-and-mixed-lists.md
    sleep 20
 done
